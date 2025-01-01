3f89c38ede2f7cc8fff3d40b970fba37fc31ff37
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-01-01 04:30:09 +0100

    chore: correction to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mortgage-repayment-calculator"
+  base: "/mortgage-repayment-calculator/",
 })
